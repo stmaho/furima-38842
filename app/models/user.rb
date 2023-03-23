a78672793571a39e4c0c06239326ce2d95535cc0
stmaho <121847812+stmaho@users.noreply.github.com>
2023-03-23 23:11:22 +0900

    ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
         
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :read_family, presence: true
-  validates :read_first, presence: true
   validates :birth_day, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+    validates :read_family, presence: true
+    validates :read_first, presence: true
+  end
+  
 end
