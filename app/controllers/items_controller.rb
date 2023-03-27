b12e7938ef001628ab72789a605cdc38acfcad45
stmaho <121847812+stmaho@users.noreply.github.com>
2023-03-27 20:57:26 +0900

    アクティブハッシュの設定、バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,3 +6,9 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 end
+
+private
+
+def message_params
+  params.require(:item).permit(:name, :content, :price, :category_id, :condition_id, :shopping_cost_id, :prefecture_id, :preparation_id,:image).merge(user_id: current_user.id)
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belong_to :category
+  belong_to :condition
+  belong_to :shopping_cost
+  belong_to :prefecture
+  belong_to :preparation
+  has_one_attached :image
+
+
+  validates :name,    presence: true
+  validates :content, presence: true
+  validates :price,   presence: true
+  validates :image, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" } 
-
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shopping_cost_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :preparation_id, numericality: { other_than: 1 , message: "can't be blank" }
 end
