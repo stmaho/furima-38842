f818719e50f0f3d368db051448f91c14855c2641
stmaho <121847812+stmaho@users.noreply.github.com>
2023-04-03 22:43:39 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
 
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.order("created_at DESC")
   end
 
   def new
