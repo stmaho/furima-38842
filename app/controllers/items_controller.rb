293fe7855c55dfb200741d63c867a604d7a34d80
stmaho <121847812+stmaho@users.noreply.github.com>
2023-04-06 16:29:45 +0900

    編集画面への遷移・編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+
 end
 
 
